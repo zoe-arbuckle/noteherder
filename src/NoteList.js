9b24a223ee778f52c00e1d339ab5feb33bc43491
Zoe Arbuckle <zoe.arbuckle@me.com>
2017-06-14 19:04:05 -0400

    Add a delete button to noteList that is not yet functional

--- src/NoteList.js
+++ src/NoteList.js
@@ -6,17 +6,17 @@ import Note from './Note'
 const noteArr = [
     {
         title: "Citizens of distant epochs",
-        text: "Sea of Tranquility the ash of stellar alchemy vastness is bearable only through love bits of moving fluff are creatures of the cosmos, consciousness a still more glorious dawn awaits two ghostly white figures in coveralls and helmets are soflty dancing tingling of the spine, concept of the number one brain is the seed of intelligence are creatures of the cosmos?",       
+        text: "Sea of Tranquility the ash of stellar alchemy vastness is bearable only through love bits of moving fluff are creatures of the cosmos, consciousness a still more glorious dawn awaits two ghostly white figures in coveralls and helmets are soflty dancing tingling of the spine, concept of the number one brain is the seed of intelligence are creatures of the cosmos?",
     },
 
     {
         title: "Preserve and cherish that pale blue dot ",
-        text: "network of wormholes a billion trillion the only home we've ever known light years dream of the mind's eye. Intelligent beings!",       
+        text: "network of wormholes a billion trillion the only home we've ever known light years dream of the mind's eye. Intelligent beings!",
     },
 
     {
         title: "Laws of physics",
-        text: "Cambrian explosion radio telescope, circumnavigated citizens of distant epochs brain is the seed of intelligence two ghostly white figures in coveralls and helmets are soflty dancing galaxies inconspicuous motes of rock and gas",       
+        text: "Cambrian explosion radio telescope, circumnavigated citizens of distant epochs brain is the seed of intelligence two ghostly white figures in coveralls and helmets are soflty dancing galaxies inconspicuous motes of rock and gas",
     },
 
 ]
@@ -24,10 +24,14 @@ const noteArr = [
 const NoteList = () => {
     return (
         <div className="NoteList">
-            <h3>Notes</h3>
+            <h3>Notes <button className="delete button" onClick>
+                <i className="fa fa-trash-o"></i>
+            </button>
+            </h3>
+
             <ul id="notes">
-                {noteArr.map((noteInfo, i) => 
-                    <Note noteInfo = {noteInfo} key = {i}/>)}
+                {noteArr.map((noteInfo, i) =>
+                    <Note noteInfo={noteInfo} key={i} />)}
 
             </ul>
         </div>
