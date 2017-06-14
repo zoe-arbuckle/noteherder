8927d3069d61bc0a8cac323ba0dd491edd80e3bc
Zoe Arbuckle <zoe.arbuckle@me.com>
2017-06-14 16:11:19 -0400

    Add a sidebar component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
+import Sidebar from './Sidebar'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        NoteHerder
+        <Sidebar />
       </div>
     );
   }
---
+++ src/Sidebar.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Sidebar = () =>{
+    return(
+        <p>Sidebar</p>
+    )
+}
+
+export default Sidebar
