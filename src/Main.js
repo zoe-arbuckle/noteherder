199d63b6c04214cf7a30370e14125f8158a87993
Zoe Arbuckle <zoe.arbuckle@me.com>
2017-06-14 16:29:37 -0400

    Add a main class, move images for sidebar, and use the local images instead of urls for sidebar

---
+++ src/Main.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Sidebar from './Sidebar'
+import NoteList from './NoteList'
+import NoteForm from './NoteForm'
+
+const Main = () => {
+    return (
+        <div>
+            <Sidebar />
+            <NoteList />
+            <NoteForm />
+        </div>
+    )
+}
+
+export default Main
--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,9 +1,21 @@
 import React from 'react'
+import quill from './quill.svg'
+import newHover from './new-hover.png'
+import newIcon from './new.png'
 
-const Sidebar = () =>{
-    return(
-        <p>Sidebar</p>
+const Sidebar = () => {
+    return (
+        <nav className="Sidebar">
+            <div className="logo">
+                <img src={quill} alt="Noteherder" />
+            </div>
+            <button className="new-note">
+                <img src={newHover} alt="New note" />
+                <img className="outline" src={newIcon} />
+            </button>
+        </nav>
     )
+
 }
 
 export default Sidebar
