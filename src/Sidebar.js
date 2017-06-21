20c309fd07807b431539be55041aba760ffa155b
Zoe Arbuckle <zoe.arbuckle@me.com>
2017-06-21 17:49:23 -0400

    Modify sidebar to have a link when adding new button

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,21 +1,24 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+
 import './Sidebar.css'
 import SignOut from './SignOut'
-
 import quill from './quill.svg'
 import newHover from './new-hover.png'
 import newIcon from './new.png'
 
-const Sidebar = (props) => {
+const Sidebar = ({ signOut }) => {
     return (
         <nav className="Sidebar">
             <div className="logo">
                 <img src={quill} alt="Noteherder" />
             </div>
-            <button className="new-note" onClick={props.createNewNote}>
-                <img src={newHover} alt="New note" />
-                <img className="outline" src={newIcon} />
-            </button>
+            <Link to='/notes' className='new-note'>
+                <button>
+                    <img src={newHover} alt="New note" />
+                    <img className="outline" src={newIcon} />
+                </button>
+            </Link>
             <SignOut signOut={props.signOut} />
         </nav>
     )
