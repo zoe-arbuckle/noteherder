8a4d346d402b3e318cf55464c7c40814b29c225d
Zoe Arbuckle <zoe.arbuckle@me.com>
2017-06-21 17:32:32 -0400

    Rewrite Main.js to work with current App.js version

--- src/Main.js
+++ src/Main.js
@@ -1,30 +1,24 @@
 import React from 'react'
-import './Main.css'
 import { Switch, Route } from 'react-router-dom'
+
+import './Main.css'
 import Sidebar from './Sidebar'
 import NoteList from './NoteList'
 import NoteForm from './NoteForm'
 
 const Main = (props) => {
-
     return (
         <div className="Main">
-            <Sidebar createNewNote={props.createNewNote}
-                signOut={props.signOut} />
-            <NoteList notes={props.notes}
-                delete={props.delete}
-                changeSelected={props.changeSelected} />
+            <Sidebar signOut={props.signOut} />
+            <NoteList notes={props.notes} />
+
             <Switch>
-                <Route path='/notes/:id' render={(navProps) =>
-                    <NoteForm notes={props.notes}
-                        saveNote={props.saveNote}
-                        selected={props.selected}
-                        {...navProps} />} />
-                <Route path='/notes' render={(navProps) =>
-                    <NoteForm notes={props.notes}
-                        saveNote={props.saveNote}
-                        selected={props.selected}
-                        {...navProps} />} />
+                <Route path="/notes/:id" render={(navProps) => (
+                    <NoteForm {...props} {...navProps} />
+                )} />
+                <Route path="/notes" render={(navProps) => (
+                    <NoteForm {...props} {...navProps} />
+                )} />
             </Switch>
 
 
