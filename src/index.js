7abba8066d7cb66b600a130bbaae63f03d8fc89d
Zoe Arbuckle <zoe.arbuckle@me.com>
2017-06-22 14:34:07 -0400

    Configure app for firebase hosting

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { HashRouter, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
 ReactDOM.render(
-    <HashRouter>
+    <Router>
         <Route component={App} />
-    </HashRouter>
+    </Router>
     , document.getElementById('root'));
 registerServiceWorker();
