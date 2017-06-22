c06ce51ac456104c795d334b94716601a7a76073
Zoe Arbuckle <zoe.arbuckle@me.com>
2017-06-22 14:15:27 -0400

    Use hashrouter for github pages

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { HashRouter, Route } from 'react-router-dom'
 
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
 ReactDOM.render(
-    <Router>
+    <HashRouter>
         <Route component={App} />
-    </Router>
+    </HashRouter>
     , document.getElementById('root'));
 registerServiceWorker();
